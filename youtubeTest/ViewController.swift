caede0bcca64a4a99c3544b37327f6f10365f777
Lai Evan <evanlai@geovision.com.tw>
2017-09-26 14:13:04 +0800

    Initial Commit

---
+++ youtubeTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youtubeTest
+//
+//  Created by Lai Evan on 9/26/17.
+//  Copyright © 2017 Lai Evan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
